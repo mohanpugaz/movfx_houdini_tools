2b4c5761e957953c5e77cc2826aff004b89403fd
Mohan Pugaz <mohanpugaz@gmail.com>
2025-09-03 16:04:31 +0530

    added file explre for alembic

---
+++ scripts/python/toolsByNodes_lib/Sop/alembic/explore.py
@@ -0,0 +1,26 @@
+# sample.py
+# copy this file and edit the tool name and function for each buttons, dont change the name of the function 
+# it must be tool_function() to work
+
+TOOL_NAME = "Explore Output Dir"
+
+
+def tool_function():
+    import hou
+    import os
+    nodes = hou.selectedNodes()
+    for node in nodes:
+        filepath = os.path.dirname(node.parm("fileName").eval())
+        open_with_default(filepath)
+    
+def open_with_default(filepath):
+    import os
+    import subprocess
+    import platform
+    system = platform.system()
+    if system == "Windows":
+        os.startfile(filepath)  # uses default program
+    elif system == "Darwin":  # macOS
+        subprocess.run(["open", filepath])
+    else:  # Linux, BSD etc
+        subprocess.run(["xdg-open", filepath])
